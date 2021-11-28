83838e366fa4cd3c42c6ab8772cd67663d6aef33
김종현 <wlwhsdos159@kookmin.kr>
2021-11-29 02:04:22 +0900

    create group

--- app/src/main/java/kr/taemin/urikiri/MainActivity.java
+++ app/src/main/java/kr/taemin/urikiri/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setTitle("                  위치공유그룹");
+        getSupportActionBar().setTitle("위치공유그룹");
 
         Button buttonOpen = (Button) findViewById(R.id.open) ;
         buttonOpen.setOnClickListener(new Button.OnClickListener() {
---
+++ app/src/main/java/kr/taemin/urikiri/group.java
@@ -0,0 +1,16 @@
+package kr.taemin.urikiri;
+
+public class group {
+    public String title;
+    public String subtitle;
+    public boolean isOn;
+
+    group(String title,String subtitle,boolean isOn)
+    {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.isOn = isOn;
+    }
+
+
+}
